4d6d5534ca5cadf5a9051c4a3ecadd14413a78b8
tsurubaso <tsurubaso@gmail.com>
2023-05-09 19:02:44 +0900

    front end terminée

--- client/index.js
+++ client/index.js
@@ -3,8 +3,12 @@ loadHTMLTable([])
 });
 
 function loadHTMLTable(data) {
+    const table= document.querySelector('table tbody');
+    //let tableHtml='';
     if (data.length == 0) { //===  
+        table.innerHTML='<tr><td class="no-data" colspan="5">No Data</td></tr>';
 
-    }
+
+    } 
 }
 
